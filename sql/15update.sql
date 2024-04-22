662e71c03ec2033ac4d559d29da138698fec213a
huitopia <huitopia27@gmail.com>
2024-04-22 14:12:28 +0900

    SQL - UPDATE SET

---
+++ sql/15update.sql
@@ -0,0 +1,42 @@
+USE w3schools;
+
+# UPDATE 테이블명
+# SET field = 변경할 값, field = 변경할 값,,,
+# WHERE row 조건
+
+# UPDATE 전 같은 WHERE 여부 확인하기
+
+SELECT *
+FROM Customers
+WHERE CustomerID = 10;
+
+UPDATE Customers
+SET Address    = 'STARK TOWER',
+    PostalCode = '12345'
+WHERE CustomerID = 10;
+
+UPDATE Customers
+SET ContactName = 'TONY STARK'
+WHERE CustomerID = 10;
+
+UPDATE Customers
+SET CustomerName = 'IRON MAN'
+WHERE CustomerID = 10;
+
+SELECT *
+FROM Customers
+WHERE Country = 'UK';
+
+UPDATE Customers
+SET Country = '영국'
+WHERE Country = 'UK';
+
+SELECT *
+FROM Customers
+WHERE Country = '영국';
+
+SELECT *
+FROM Customers;
+UPDATE Customers
+SET Country = '영국';
+
