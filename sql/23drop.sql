13b9797ba667925942f3d7a910f409f0f4b7dfb1
huitopia <huitopia27@gmail.com>
2024-04-25 11:13:40 +0900

    DROP

---
+++ sql/23drop.sql
@@ -0,0 +1,16 @@
+USE w3schools;
+
+CREATE TABLE table99
+(
+    col1 INT
+);
+# DROP TABLE : 테이블 삭제
+DROP TABLE table99;
+SELECT *
+FROM table99; # 없는 테이블이라 조회 불가
+
+CREATE TABLE table100
+(
+    col1 INT
+);
+DROP TABLE table100;
