33bc14e2c1cc34c3656c8531a4e1c6a5bb61e5e1
huitopia <huitopia27@gmail.com>
2024-05-20 16:22:44 +0900

    @RestController

--- src/main/java/com/study/controller/Controller45.java
+++ src/main/java/com/study/controller/Controller45.java
@@ -1,23 +1,21 @@
 package com.study.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 
-@Controller
+@RestController // Controller + ResponseBody
 @RequestMapping("/api/main45")
 @RequiredArgsConstructor
 public class Controller45 {
 
     @PostMapping("sub1")
-    @ResponseBody
     public String sub1(@RequestParam("name") String name,
                        @RequestParam("file") MultipartFile file) throws IOException {
         System.out.println("name = " + name);
@@ -28,7 +26,6 @@ public class Controller45 {
     }
 
     @PostMapping("sub2")
-    @ResponseBody
     public String sub2(@RequestParam("name") String name,
                        @RequestParam("file[]") MultipartFile[] files) throws IOException {
         System.out.println("name = " + name);
